efcc00f543ffa3c526ba4652b3479b55fc47c09c
Ameer-Zubedat <ameer_z_90@hotmail.com>
2019-05-30 23:43:14 +0300

    changes

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -61491,7 +61491,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56894" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57044" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
